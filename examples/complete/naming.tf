aa6a4c636d0cb02cb1727ea4e9c15718633e9203
Akash Varshney Personal <akash.varshney@arcadis.com>
2024-08-30 12:13:36 +0100

    fix: added naming_ types  entry for eventhub_authorization_rule

--- examples/complete/naming.tf
+++ examples/complete/naming.tf
@@ -4,5 +4,5 @@ locals {
     for type in local.naming_types : type => lookup(module.naming, type).name
   }
 
-  naming_types = ["eventhub", "eventhub_consumer_group", "eventhub_namespace_authorization_rule"]
+  naming_types = ["eventhub", "eventhub_consumer_group", "eventhub_namespace_authorization_rule", "eventhub_authorization_rule"]
 }
